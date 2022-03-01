6e8304ed92ec618c2e3eb656fa445c8f752d0ccb
leen60 <laurencija.zilinskaite@gmail.com>
2022-03-01 10:38:17 +0200

    Initial commit

---
+++ src/main/java/com/p2p/p2pchatserver/P2pChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.p2p.p2pchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2pChatServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/p2p/p2pchatserver/P2pChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2p.p2pchatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2pChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
